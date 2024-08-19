9e2d1effccb908e2c4d308ce1838e64f87293f69
Andrei Bondarev <andrei.bondarev13@gmail.com>
2024-08-19 15:13:00 -0400

    update lib nam

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='inline-project',
+    name='inline_project',  # Changed from 'inline-project' to 'inline_project'
     version='0.1',
-    packages=find_packages(),
+    py_modules=['inline_project'],  # Add this line to specify the module
     entry_points={
         'console_scripts': [
             'inline-project=inline_project:main',
@@ -15,11 +15,12 @@ setup(
     description='A tool to inline project content into a single file',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/inline-project',
+    url='https://github.com/andreibondarev/inline_project',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
 )
+
 
